c2a89e629f4f726ce6cd3b063c08131d6cd01f77
Kenmusembi <kenmusembi21@gmail.com>
2020-02-27 16:52:37 +0300

    added a snackbar to the main activity

--- app/src/main/java/com/example/myapplicationone/MainActivity.java
+++ app/src/main/java/com/example/myapplicationone/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.myapplicationone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button mSnackbarBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSnackbarBtn = findViewById(R.id.snackbar_btn);
+
+        mSnackbarBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar snackbar = Snackbar.make(v, "You Just clicked this", Snackbar.LENGTH_LONG);
+
+                snackbar.show();
+            }
+        });
+
     }
 }
